860f43b0e935b03881ec1e14684eacf6fa9bdb77
Alejandro Lopez Rubio <alex.lopez.rubio2600@gmail.com>
2024-04-29 13:09:49 -0600

    generador qr
    qr

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeUrl, SafeValue } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'qrProject';
+  title = 'QR Project ';
+  qrdata = 'hola mundo';
+  qrCodeDownloadLink: SafeUrl ='';
+
+  onChange(url: SafeUrl){
+    this.qrCodeDownloadLink = url;
+  }
 }
